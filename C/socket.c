685941a171b11ecfae783800087628d2d5e36ada
lystiger <anhdh.23bi14015@usth.edu.vn>
2025-11-22 14:20:34 +0700

    Add some comments on socket.c for more understandable code

--- C/socket.c
+++ C/socket.c
@@ -7,8 +7,14 @@
 
 int socket(int domain, int type, int protocol){
     int fd = syscall(__NR_socket, domain, type, protocol); //fd = file descriptors
+    //__NR_socket: Since i have a petit brain, i will explain within my understanding.
+    //             In linux every syscall has an unique number and __NR_socket is the
+    //             number that assigned to the socket() syscall
+    // So when i do int fd = ... => im telling the kernel to run the syscall for creating
+    // a __NR_socket with arguments like domain, type and protocol, that's is. 
     if (fd < 0){
         perror("socket syscall failed");
+        // If error it will print socket syscall failed: Connection refused
         return -1;
     } else {
     return fd;
